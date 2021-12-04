bdccc299ee78ed97eb2ea04b2a7966e61f795a4b
Noshua <noshuas@gmail.com>
2021-12-04 12:48:06 -0800

    mongo set up with new auth system. Google added as an oAuth Provider

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,9 +1,34 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
 
 export default NextAuth({
-
+  adapter: MongoDBAdapter(clientPromise),
+  events: {
+    async signIn(message) {
+      console.log('\x1b[31m%s\x1b[0m', 'signIn:')
+      console.log(message)},
+    async signOut(message) {
+      console.log('\x1b[31m%s\x1b[0m', 'signOut:')
+      console.log(message)},
+    async createUser(message) {
+      console.log('\x1b[31m%s\x1b[0m', 'createUser:')
+      console.log(message)},
+    async updateUser(message) {
+      console.log('\x1b[31m%s\x1b[0m', 'updateUser:')
+      console.log(message)},
+    async linkAccount(message) {
+      console.log('\x1b[31m%s\x1b[0m', 'linkAccount:')
+      console.log(message)},
+    async session(message) {
+      console.log('\x1b[31m%s\x1b[0m', 'session:')
+      console.log(message)},
+    async error(message) {
+      console.log('\x1b[31m%s\x1b[0m', 'error:')
+      console.log(message)}
+  },
   providers: [
     // GithubProvider({
     //   clientId: process.env.GITHUB_ID,
@@ -14,6 +39,7 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
+
 
     // LinkedInProvider({
     //   clientId: process.env.LINKEDIN_CLIENT_ID,
