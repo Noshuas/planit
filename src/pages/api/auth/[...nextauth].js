95483b87d1584d7abb74d155b0ea6050e52af8e8
Noshua <noshuas@gmail.com>
2021-12-05 02:54:40 -0800

    added jsconfig.json for cleaning up import paths even further

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
-import clientPromise from "../../../lib/mongodb"
+import clientPromise from "lib/database/mongodb"
 
 export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
