802b467dffd840c18fa5a1559500b99b3f7748b2
Noshua <noshuas@gmail.com>
2021-12-22 10:43:46 -0800

    the index page now pushes directly to the home page where session validity will be checked

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,12 @@
-import LoadingSkeleton from 'components/LoadingSkeleton';
-import { signIn, useSession } from "next-auth/react";
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export const Index = () => {
-  const { status } = useSession({required: true, onUnauthenticated: signIn})
   const router = useRouter();
 
-  if (status === 'authenticated')
-    router.push('./home')
+  useEffect(()=>{router.push('./home')})
 
-  return <LoadingSkeleton />
+  return ''
 }
 
 export default Index;
